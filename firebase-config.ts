4934de4a3a60c8636262d34e8756297954865dbc
Thomas <152863307+Thomas-TF1@users.noreply.github.com>
2024-05-14 16:51:48 +0200

    add app entry

--- firebase-config.ts
+++ firebase-config.ts
@@ -1,6 +1,6 @@
 import admin, { ServiceAccount, initializeApp } from "firebase-admin";
 
-import serviceAccount from "./backoffice-cdb4e-firebase-adminsdk-ch78t-17511cbc9d.json";
+import serviceAccount from "./secrets/serviceAccount.json";
 
 export const app = initializeApp({
   credential: admin.credential.cert(serviceAccount as ServiceAccount),
